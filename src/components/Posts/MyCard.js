39a939aec59d4ca7cbf52f40292b8a0094105626
Morgan <morgan.thompson@advance-auto.com>
2020-05-04 11:26:36 -0400

    ui styles

--- src/components/Posts/MyCard.js
+++ src/components/Posts/MyCard.js
@@ -11,7 +11,7 @@
   const MyCard = (post) => {
     Moment.locale('en');
     return (
-      <Card className={'MuiProjectCard--01'} style={{width: '400px'}}>
+      <Card className={'MuiProjectCard--01'}>
         <CardMedia
           className={'MuiCardMedia-root'}
           style={{height: "300px"}}
