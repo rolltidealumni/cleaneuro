cdaad72f2db06779ca9be2d758a93df1e4972df5
Morgan <morgan.thompson@advance-auto.com>
2020-05-07 14:19:20 -0400

    analytics config

--- src/firebase/firebase-config.js
+++ src/firebase/firebase-config.js
@@ -4,5 +4,7 @@ export default {
   databaseURL: "https://gagunk-app.firebaseio.com",
   projectId: "gagunk-app",
   storageBucket: "gagunk-app.appspot.com",
-  messagingSenderId: "214279252823"
+  messagingSenderId: "214279252823",
+  appId: "1:214279252823:web:d97b07d9c4d3684878ec12",
+  measurementId: "G-C9CM4WXRJ7"
 };
