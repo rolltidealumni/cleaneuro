cbe4629cc1da0a9da89a436ea0960c34dd346e76
Morgan <morgan.thompson@advance-auto.com>
2020-08-01 17:54:19 -0400

    removing unused code

--- src/components/Critique.js
+++ src/components/Critique.js
@@ -24,55 +24,10 @@ const Critique = (props) => {
   const [loading, setLoading] = useState(false);
   const [rating, setRating] = useState(0);
   const [comment, setComment] = useState("");
-  // const [chipsTouched, setChipsTouched] = useState([]);
-  // let chips = [
-  //   {key: 0, label: "Lighting"},
-  //   {key: 1, label:"Color"},
-  //   {key: 2, label:"Composition"},
-  //   {key: 3, label:"Emotion"},
-  //   {key: 4, label:"Focus"},
-  //   {key: 5, label:"Concept"},
-  //   {key: 6, label:"Crop"},
-  //   {key: 7, label:"Perspective"}
-  // ];
-
-  // const selectChip = (chip) => {
-  //   chipsTouched.push({key: chip.key, label: chip.label});
-  //   setChipsTouched(chipsTouched);
-  // }
-
-  // const deSelectChip = (chip) => {
-  //   setChipsTouched((chipsTouched) => chipsTouched.filter((x) => x.key !== chip.key));
-  // }
-
-  // const handleSubmit = (e) => {
-  //   let postsRef = realTime.ref("posts");
-  //   setLoading(true);
-  //   postsRef.push({
-  //     location: jquery('#combo-box-demo').val(),
-  //     submitted: new Date().toString(),
-  //     oneStar: 0,
-  //     twoStars: 0,
-  //     threeStars: 0,
-  //     fourStars: 0,
-  //     fiveStars: 0,
-  //     total: 0,
-  //   });
-  // };
 
   const changeRating = (newRating, name) => {
     setRating(newRating);
   };
-
-  // const isSelected = (chip) => {
-  //   const selected = chipsTouched.filter((x) => x.key === chip.key).length > 0; 
-  //   if (selected) deSelectChip(chip);
-  //   else selectChip(chip);
-  // };
-
-  // const chipFocus = (chip) => {
-  //    return chipsTouched.filter((x) => x.key === chip.key).length > 0; 
-  // }
 
   const updateRating = () => {
     let postRef = realTime.ref("posts/" + props.post.key);
@@ -325,52 +280,6 @@ const Critique = (props) => {
               </li>
             </ul>
           </span>}
-        {/* <div style={{marginTop: '20px'}}>
-            {chips.map(chipy => {
-              return (
-                <Chip
-                  onClick={() => isSelected(chipy)}
-                  mode="outlined"
-                  key={chipy.key}
-                  label={
-                    !chipFocus(chipy) ?
-                      <span style={{
-                        color: chipFocus(chipy) ? '#FBC02D !important' : 'black !important',
-                        fontWeight: chipFocus(chipy) ? '500 !important' : 'normal !important'
-                      }}>
-                        {chipy.label}
-                        <img
-                          alt="heart"
-                          src={heartEmpty}
-                          width="18px"
-                          style={{ verticalAlign: "middle", marginRight: "3px", marginLeft: '18px', width: '15px' }}
-                        />
-                      </span> :
-                      <span style={{
-                        color: chipFocus(chipy) > 0 ? '#FBC02D !important' : 'black !important',
-                        fontWeight: chipFocus(chipy) > 0 ? '500 !important' : 'normal !important'
-                      }}>
-                        {chipy.label}
-                        <img
-                          alt="heart"
-                          src={heartFill}
-                          width="18px"
-                          style={{ verticalAlign: "middle", marginRight: "3px", marginLeft: '18px', width: '15px' }}
-                        />
-                      </span>
-                  }
-                  style={{
-                    margin: "5px",
-                    height: "35px",
-                    backgroundColor: 'white',
-                    borderColor: chipFocus(chipy) ? '#FBC02D' : 'rgb(186, 186, 186)', 
-                    borderWidth: "1px",
-                    borderStyle: "solid"
-                  }}
-                />
-              )
-            })}
-          </div> */}
         {props.user.uid !== props.post.author ?
           <center>
             <FlatButton
